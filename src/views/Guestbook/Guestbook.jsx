c5ccf415dbf03588d4e4eabdc536b561a96bd6e8
armaBurton <armanarma@armaburton.com>
2022-05-07 13:02:49 -0700

    more incremental progress, nothing broken again, I don't know what cause this to fail so spectacularly

--- src/views/Guestbook/Guestbook.jsx
+++ src/views/Guestbook/Guestbook.jsx
@@ -1,5 +1,25 @@
 import { useEffect } from 'react';
+import { useGuestbook } from '../../context/GuestbookProvider';
+import { getEntries } from '../../services/entries';
 
-export default function Guestbook() {
-  return <></>;
+export default function Guestbook({ children, ...rest }) {
+  const { logout } = useGuestbook();
+  const { entries, setEntries, loading, setLoading } = useGuestbook();
+
+  useEffect(() => {
+    setLoading(true);
+
+    getEntries()
+      .then(setEntries)
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1>Guestbook</h1>
+      <p>You should only see this page if you're logged in</p>
+      <button onClick={logout}>Sign out</button>
+    </>
+  );
 }
