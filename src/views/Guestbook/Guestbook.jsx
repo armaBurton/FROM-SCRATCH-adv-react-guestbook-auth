d3c20a50857a8b5da4a857f6dd69bd35945972c9
armaBurton <armanarma@armaburton.com>
2022-05-07 13:08:19 -0700

    making a commit because I have to go slaughter chickens

--- src/views/Guestbook/Guestbook.jsx
+++ src/views/Guestbook/Guestbook.jsx
@@ -20,6 +20,15 @@ export default function Guestbook({ children, ...rest }) {
       <h1>Guestbook</h1>
       <p>You should only see this page if you're logged in</p>
       <button onClick={logout}>Sign out</button>
+      {loading ? (
+        <p>Loading entries...</p>
+      ) : (
+        <ul>
+          {entries.map((entry) => (
+            <li key={entry.id}>{entry.content}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
